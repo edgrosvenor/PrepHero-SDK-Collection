e5f11b94c8241cacf569ac6ceac0990d293e3730
Xinjiang Shao <shaoxinjiang@gmail.com>
2012-07-25 16:15:42 -0500

    Added PrepHeroApi Class and demo to get access to a secret resource

---
+++ php/PrepHeroApi.php
@@ -0,0 +1,95 @@
+<?php
+    /**
+      *  Author          : Xinjiang Shao <Xinjiang.Shao@exactsports.com>
+      *  Date            : July 2012
+      *  Description     : This file provide a wrapper class for clients to 
+      *                    use OAuth2 authentication and acsess secret resources 
+      *                    in PrepHero.com
+      **/
+      
+require(dirname(__FILE__).'/Client.php');
+require(dirname(__FILE__).'/GrantType/IGrantType.php');
+require(dirname(__FILE__).'/GrantType/AuthorizationCode.php');
+
+const CLIENT_ID                 = 'Shao';
+const CLIENT_SECRET             = '994738dd31ed445ac27f';
+const REDIRECT_URI              = 'http://soleoshao.com/sandbox/oauth2/demo.php';
+
+const ACCESS_TOKEN_BEARER       = 1;
+
+
+class PrepHeroApi{
+    
+                                                   
+    protected $base_url                  = "https://dev.prephero.com/index.php?module=prephero&type=user&func=v1";
+    
+    protected $client_id;
+    protected $client_secret;
+    protected $redirect_url;
+    protected $authorization_endpoint;
+    protected $token_endpoint;
+    
+    protected $client;
+    private   $code;
+    private   $returntype;
+    private   $access_token;
+    /**
+     * @param string $returntype JSON or XML, by default JSON
+     */
+    function __construct($returntype = 'json'){
+        
+        $this->client_id                = CLIENT_ID;
+        $this->client_secret            = CLIENT_SECRET;
+        $this->redirect_uri             = REDIRECT_URI;
+        
+        $this->authorization_endpoint   = $this->base_url.'&method=authorize&return='. $returntype;
+        $this->token_endpoint           = $this->base_url.'&theme=raw&method=grant&return='. $returntype;
+        $this->returntype               = $returntype;
+        $this->client = new OAuth2\Client($this->client_id, $this->client_secret);
+        session_start();
+    }
+    
+    /**
+     * @return string url for authentication. choose allow or deny 
+     */
+    public function getAuthUrl(){
+        $auth_url = $this->client->getAuthenticationUrl($this->authorization_endpoint, $this->redirect_uri);
+        return $auth_url;
+    }
+    /**
+     * @param string $code retrieve from $_GET['code'] on redirect_uri
+     * @return array ['result']        for xml or json response. In result, we have information of access_token,expires_in,token_type, scope, refresh_token
+     *               ['code']          for http status
+     *               ['content_type']  for http type eg. application/json or application/xml
+     */
+    public function setAccessToken($code){
+        $params = array('code' => $code, 'redirect_uri' => $this->redirect_uri);
+        $response = $this->client->getAccessToken($this->token_endpoint, 'authorization_code', $params);
+        if(!isset($response['result']['error'])){
+            $this->access_token = $response['result']['access_token'];
+            setcookie('prephero_refresh_token',  $response['result']['refresh_token']);
+        }
+        else{
+            if($response['result']['error'] == "invalid_grant"){
+                // get another token
+                $params = array('refresh_token' => $_COOKIE['prephero_refresh_token']);
+                $response = $this->client->getAccessToken($this->token_endpoint, 'refresh_token', $params);
+                print_r($response);
+                $this->access_token = $response['refresh_token'];
+            }
+        }
+        return $response;
+    }
+    
+    public function fetch($method, $secretresource = null){
+		$this->client->setAccessToken($this->access_token);
+        $response = $this->client->fetch($this->base_url.'&theme=raw&method='.$method.'&return='.$this->returntype);
+        return $response;
+    }
+    
+    public function getProfile(){
+        return self::fetch('accessresources', null);
+    }
+    
+    
+}          
--- php/demo.php
+++ php/demo.php
@@ -1,43 +1,28 @@
 <?php
-    require(dirname(__FILE__).'/Client.php');
-    require(dirname(__FILE__).'/GrantType/IGrantType.php');
-    require(dirname(__FILE__).'/GrantType/AuthorizationCode.php');
+include_once "PrepHeroApi.php";
 
+$api = new PrepHeroApi();
 
-define('CLIENT_ID','Shao');
-define('CLIENT_SECRET','994738dd31ed445ac27f');
-define('REDIRECT_URI', 'http://soleoshao.com/sandbox/oauth2/demo.php');
-define('AUTHORIZATION_ENDPOINT', 'https://dev.prephero.com/index.php?module=prephero&type=user&func=v1&method=authorize&return=json');
-define('TOKEN_ENDPOINT', 'https://dev.prephero.com/index.php?module=prephero&type=user&func=v1&theme=raw&method=grant&return=json');
-const ACCESS_TOKEN_BEARER   = 1;
-/*
-* not good with php 5.2
-    const CLIENT_ID     = 'Shao';
-    const CLIENT_SECRET = '994738dd31ed445ac27f';
-
-    const REDIRECT_URI           = 'http://soleoshao.com/sandbox/oauth2/demo.php';
-    const AUTHORIZATION_ENDPOINT = 'https://dev.prephero.com/oauth/authorize';
-    const TOKEN_ENDPOINT         = 'https://dev.prephero.com/oauth/access_token';
-*/
-    $client = new OAuth2\Client(CLIENT_ID, CLIENT_SECRET);
     if (!isset($_GET['code']))
     {
-        $auth_url = $client->getAuthenticationUrl(AUTHORIZATION_ENDPOINT, REDIRECT_URI);
+        $auth_url = $api->getAuthUrl();
         header('Location: ' . $auth_url);
         die('Redirect');
     }
     else
     {
-        $params = array('code' => $_GET['code'], 'redirect_uri' => REDIRECT_URI);
-        $response = $client->getAccessToken(TOKEN_ENDPOINT, 'authorization_code', $params);
-		echo "<pre>";
-		     var_dump($response);
+        $response = $api->setAccessToken($_GET['code']);
+        echo "<pre>";
+        var_dump($response);
 		echo "</pre>";
-        $client->setAccessToken($response['result']['access_token']);
-        $response = $client->fetch('https://dev.prephero.com/index.php?module=prephero&type=user&func=v1&theme=raw&method=accessresources&return=json');
-echo "<pre>";
-        var_dump($response, $response['result']);
-echo "</pre>";
+		for ($i=0;$i<20;$i++){
+			$response = $api->getProfile();
+			echo "<pre>";
+	        var_dump($response);
+			echo "</pre>";
+			sleep(1);
+		}
+        
     }
 
 ?>
